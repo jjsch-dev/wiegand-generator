a4431c2077f3bd6e317ee66e37f88fc79128312e
jjsch-dev <juanjoseschiavoni@hotmail.com>
2020-11-04 10:36:02 -0300

    the output format was added

--- wieg_gen.py
+++ wieg_gen.py
@@ -12,40 +12,70 @@ import platform
 parser = argparse.ArgumentParser(description="wiegand generator for MC-100")
 parser.add_argument('--version', action='version', version='%(prog)s 0.1.2')
 parser.add_argument("-p", "--port", required=True, help="serial communication port (win = COMxxx, linux = ttyXXXX)")
+parser.add_argument("-o", "--output", type=str, required=True, help="specify the output string format, std26 = wiegand 26-bit standard, mif32 = mifare 32-bit.")
 parser.add_argument("-f", "--facility", type=int, required=True, help="0 to 255")
 parser.add_argument("-i", "--identifier", type=int, required=True, help="0 to 65535")
 parser.add_argument("-c", "--count", type=int, required=True, help="times the identifier increments")
 parser.add_argument("-d", "--delay", type=int, required=True, help="delay in mS between identifiers")
 args = parser.parse_args()
 
+id_len = 0
+
 
 def check_params():
+    global id_len
+
+    if 'std26' == args.output:
+        id_len = 0xFFFF
+    elif 'mif32' == args.output:
+        id_len = 0xFFFFFFFF
+    else:
+        return False
+
     if 0 > args.facility > 255:
         return False
 
-    if 0 > args.identifier > 65535:
+    if 0 > args.identifier > id_len:
         return False
+
     return True
 
 
 def open_port():
+    if args.port == "disabled":
+        return args.port
+
     try:
         return serial.Serial(args.port, "115200", 8, 'N', 1, timeout=1)
     except:
         return None
 
 
+def close_port(serial_device):
+    if serial_device != "disabled":
+        try:
+            serial_device.close()
+        except: pass
+
+
 def generate_codes(serial_device):
+    global id_len
+
     count = args.count
 
     for fc in range(args.facility, 255):
-        for identifier in range(args.identifier, 65535):
-            card_id = "{:03d}{:04d}".format(fc, identifier)
+        for identifier in range(args.identifier, id_len):
+
+            if args.output == 'std26':
+                card_id = "{:03d}{:04d}".format(fc, identifier)
+            else:
+                card_id = "{:010d}".format(identifier)
 
             send_id = card_id + "\n\r"
 
             try:
-                serial_device.write(send_id.encode(encoding='ascii'))
+                if serial_device != "disabled":
+                    serial_device.write(send_id.encode(encoding='ascii'))
                 print(card_id)
             except:
                 print("{} serial write error".format(card_id))
@@ -66,6 +96,7 @@ if __name__ == '__main__':
         device = open_port()
         if device:
             generate_codes(device)
+            close_port(device)
         else:
             print("Can't open the serial port")
     else:
